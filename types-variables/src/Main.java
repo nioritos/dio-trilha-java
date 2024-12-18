25d823b2d7c2a3a5673002cb6c5542a9430a62c5
Junior Guimarães <junioradr2008@gmail.com>
2024-12-18 14:42:36 -0300

    variables and types commited

--- types-variables/src/Main.java
+++ types-variables/src/Main.java
@@ -1,15 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int numberInt = 123;//integer numbers
+        double numberDouble = 123.231; //double numbers
+        float numberFloat = 123.321f; //float numbers
+        char charWord = 'R'; //one letter
+        String nameString = "Junior"; //a lot of letters
+        boolean falseValue = false; //boolean false
+        boolean trueValue = true; //boolean true
     }
 }
